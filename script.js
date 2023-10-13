80586f3fc8fc7d7bc5208a4ebfd4c2fe9653358a
Ko Khant <hh4626783@gmail.com>
2023-10-13 18:46:06 +0630

    first commit in main

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+    # git branch
+
+    // git branch --> to see how many branches you made 
+    // git switch branchName --> to change branch to another
+
+    // git branch branchName --> git switch branchName --> to make new branch
+    // git switch -c branchName --> to make a new branch
+
+   
+*/
+
+console.log('Hello world');
